7484a863cc1d262da6fe870230cf0c1b7675b0d4
Bram Mittendorff <bram.mittendorff@dealerdirect.nl>
2020-10-05 14:35:24 +0200

    First commit! :)

---
+++ README.md
@@ -0,0 +1,21 @@
+# A mongodb connector
+
+When you use an url for mongo db and you want a quick peek to see the databases and if its connecting you can use this application.
+
+## Requirements
+
+- node
+- MongoClient
+
+## Installation
+
+For the installation please run:
+```
+npm install
+```
+
+## Usage
+
+```
+node index.js "mongodb://user:pass@myserver.com"
+```# Databaseconnect
---
+++ index.js
@@ -0,0 +1,61 @@
+const { MongoClient } = require('mongodb');
+const { Client } = require('pg');
+const url = require('url');
+
+// Get url by first arg
+var myUrl = process.argv.slice(2)[0];
+
+var parsedUrl = url.parse(myUrl, true);
+
+async function mongo() {
+    const uri = myUrl;
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    try {
+        await client.connect();
+	    console.log('Connected to mongo server.');
+        await listMongoDatabases(client);
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+async function listMongoDatabases(client) {
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+async function postgres() {
+    const uri = myUrl;
+    const client = new Client(uri);
+
+    try {
+        client.connect();
+        console.log('Connected to postgres server.');
+        client.query('SELECT datname FROM pg_database', (err, res) => {
+            if (Boolean(res.rows)) {
+                console.log("Databases:");
+                res.rows.forEach(db => console.log(` - ${db.datname}`));
+            } else {
+                console.log(err, res);
+            }
+            client.end();
+        })
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+// Determin the type of protocol 
+protocol = parsedUrl.protocol.slice(0,-1);
+if (protocol == 'mongodb+srv') {
+    mongo().catch(console.error);
+} else if (protocol == 'postgres') {
+    postgres().catch(console.error)
+} else {
+    console.log('This protocol is not yet supported: ' + protocol);
+}
